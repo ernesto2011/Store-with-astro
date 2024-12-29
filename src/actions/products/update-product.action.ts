3e239e6aeb161243241c08803c4b9a8e804c1bb9
ernesto2011 <skillmx107@gmail.com>
2024-12-28 21:45:29 -0600

    prepare to send imagefiles to backend

--- src/actions/products/update-product.action.ts
+++ src/actions/products/update-product.action.ts
@@ -4,6 +4,15 @@ import { z } from "astro:schema";
 import { getSession } from "auth-astro/server";
 import {v4 as UUID } from 'uuid';
 
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGES_TYPES = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/webp",
+    "image/svg+xml"
+]
+
 export const updateProduct = defineAction({
     accept: "form",
     input: z.object({
@@ -17,6 +26,16 @@ export const updateProduct = defineAction({
         tags: z.string(),
         title: z.string(),
         type: z.string(),
+        imageFiles: z
+        .array(
+          z
+            .instanceof(File)
+            .refine((file) => file.size <= MAX_FILE_SIZE, 'Max image size 5MB')
+            .refine((file) => {
+              return ACCEPTED_IMAGES_TYPES.includes(file.type);
+            }, `Only supported image files are valid, ${ACCEPTED_IMAGES_TYPES.join(',')}`)
+        )
+        .optional(),
     }),
     handler: async (form, {request}) => {
         const session = await getSession(request);
@@ -25,7 +44,7 @@ export const updateProduct = defineAction({
             throw new Error('Unauthorized');
         }
         
-        const {id = UUID(), ...rest} = form;
+        const {id = UUID(),imageFiles, ...rest} = form;
         rest.slug = rest.slug.toLocaleLowerCase().replaceAll(' ','-').trim();
         const  product ={
             id:id,
@@ -37,6 +56,7 @@ export const updateProduct = defineAction({
         }else{
             await db.update(Product).set(product).where(eq(Product.id, id));
         }
+        console.log({product,imageFiles});
         
         return product;
         
